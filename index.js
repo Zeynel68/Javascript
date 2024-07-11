c74713655118ae5fea35856faaa3a4930e132ecb
Zeynel Hüseyni <huseynizeynel@gmail.com>
2024-07-11 18:01:51 +0300

    Şart bloklarıyla isim ve tc isteme uygulaması

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-let vize1 = Number(prompt("Vize 1 notunu giriniz"));
-let vize2 = Number(prompt("Vize 2 notunu giriniz"));
-let final = Number(prompt("Final notunu giriniz"));
+// let vize1 = Number(prompt("Vize 1 notunu giriniz"));
+// let vize2 = Number(prompt("Vize 2 notunu giriniz"));
+// let final = Number(prompt("Final notunu giriniz"));
 
-let ortalama = vize1*0.3 + vize2*0.3 + final*0.4;
+// let ortalama = vize1*0.3 + vize2*0.3 + final*0.4;
 
-if(ortalama>=60){
-alert("Dersten geçtiniz tebrikler notunuz: " + ortalama)
+// if(ortalama>=60){
+// alert("Dersten geçtiniz tebrikler notunuz: " + ortalama)
 
-}else{
- alert("Dersten kaldınız notunuz: " + ortalama)
+// }else{
+//  alert("Dersten kaldınız notunuz: " + ortalama)
+// }
+
+let ad = prompt("İsminizi giriniz : ");
+let tckn = prompt("Tc'nizi giriniz.")
+
+kontrolEt(ad , tckn)
+function kontrolEt(ad, tckn){
+    if(ad!==""){
+        if(tckn.length == 11){
+            alert("İşleminiz başarıyla tamamlanmıştır.")
+        } else{
+            alert("Lütfen TC'nizi tam bir şekilde giriniz")
+        }
+    } else{
+        alert("Lütfen isim alanını doldurunuz.");
+    }
 }
